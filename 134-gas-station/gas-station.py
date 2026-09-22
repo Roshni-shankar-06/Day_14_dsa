75905070864a5f82ba6e2859f3de97e3e0b6f447
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-22 09:34:11 +0530

    Refactor gas station algorithm logic
    Removed unnecessary reset of 'summ' and 'ans' assignment.

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -9,7 +9,4 @@ class Solution:
       net += gas[i] - cost[i]
       summ += gas[i] - cost[i]
       if summ < 0:
-        summ = 0
-        ans = i + 1  # Start from the next index.
-
-    return -1 if net < 0 else ans
+      

e21aa97ce5346a34cfb461a1b7946583cab005da
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-22 09:34:29 +0530

    Update gas-station.py

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,9 +1,4 @@
 class Solution:
   def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
     ans = 0
-    net = 0
-    summ = 0
-
-    # Try to start from each index.
-    for i in range(len(gas)):
-    
+  

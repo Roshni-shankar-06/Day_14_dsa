66e1a15b21656852ec3ceeb9fd882594b187f1a7
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-22 09:33:19 +0530

    Refactor bit manipulation logic for single number

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -5,6 +5,4 @@ class Solution {
     for (int i = 0; i < 32; ++i) {
       int sum = 0;
       for (final int num : nums)
-        sum += num >> i & 1;
-      sum %= 3;
-      ans |= sum << i;
+      

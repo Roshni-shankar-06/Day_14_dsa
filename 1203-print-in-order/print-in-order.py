d546bdb22784bd33ed3ab040f6d692ae8d3c6116
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-22 09:36:53 +0530

    Refactor Foo class by removing secondDone lock
    Removed secondDone lock and its acquisition from the constructor.

--- 1203-print-in-order/print-in-order.py
+++ 1203-print-in-order/print-in-order.py
@@ -4,10 +4,4 @@ from threading import Lock
 class Foo:
   def __init__(self):
     self.firstDone = Lock()
-    self.secondDone = Lock()
-    self.firstDone.acquire()
-    self.secondDone.acquire()
-
-  def first(self, printFirst: 'Callable[[], None]') -> None:
-  
-
+   

0841747dfcc57cb82185a7ea3228529d4dca399b
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-22 09:35:09 +0530

    Refactor gas station logic to simplify calculations
    Removed unnecessary reset of summ and related logic.

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -8,8 +8,4 @@ class Solution:
     for i in range(len(gas)):
       net += gas[i] - cost[i]
       summ += gas[i] - cost[i]
-      if summ < 0:
-        summ = 0
-        ans = i + 1  # Start from the next index.
-
-    return -1 if net < 0 else ans
+   
